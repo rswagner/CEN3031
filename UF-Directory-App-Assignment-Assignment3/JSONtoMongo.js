65c5e3117806c21a4df102e21630620b028821c1
Ricardo Wagner <Ricardo@Ricardos-MacBook-Pro.local>
2017-09-06 23:38:54 -0400

    init commit

---
+++ UF-Directory-App-Assignment-Assignment3/JSONtoMongo.js
@@ -0,0 +1,50 @@
+'use strict';
+/* 
+  Import modules/files you may need to correctly run the script. 
+  Make sure to save your DB's uri in the config file, then import it with a require statement!
+ */
+var fs = require('fs'),
+    mongoose = require('mongoose'), 
+    Schema = mongoose.Schema, 
+    Listing = require('./ListingSchema.js'), 
+    config = require('./config');
+
+/* Connect to your database */
+mongoose.connect(config.db.uri)
+
+/* 
+  Instantiate a mongoose model for each listing object in the JSON file, 
+  and then save it to your Mongo database 
+ */
+ var mongooseConnection = mongoose.connection;
+ mongooseConnection.on('connected', function() {
+  console.log('Mongoose default connection open to mongodb');
+  loadListings();
+ });
+
+ var listingObjects = null; 
+ var loadListings = function(){
+  fs.readFile('listings.json', 'utf8', function(err,entries){
+
+  if (err) {throw err}; 
+  else{
+    listingObjects = JSON.parse(entries);
+    pushToMongoDb(); 
+  }
+ });
+};
+
+var pushToMongoDb() = function(){
+for (var entry in listingData["entries"]) {
+        var toPush = new Listing(listingData["entries"][entry]);
+        toPush.save(function (err) {
+            if (err) {throw err}; 
+              else
+                console.log("Entry saved!");
+        });
+    };
+};
+/* 
+  Once you've written + run the script, check out your MongoLab database to ensure that 
+  it saved everything correctly. 
+ */
---
+++ UF-Directory-App-Assignment-Assignment3/queries.js
@@ -0,0 +1,53 @@
+/* Fill out these functions using Mongoose queries*/
+
+var findLibraryWest = function() {
+  /* 
+    Find the document that contains data corresponding to Library West,
+    then log it to the console. 
+   */
+   Listing.find({name: 'Library West'}, function(err, documentData){
+    if(err) {throw err}; 
+    console.log(documentData);
+   });
+};
+var removeCable = function() {
+  /*
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
+    and remove this listing from your database and log the document to the console. 
+   */
+    Listing.findOneAndRemove({code:'CABL'}, function(err, documentData) {
+      if(err) {throw err}; 
+
+        console.log(documentData);
+        console.log('CABL deleted!');
+    });
+};
+var updatePhelpsLab = function() {
+  /*
+    Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
+    log the updated document to the console. 
+   */
+       Listing.findOneAndUpdate({ name: 'Phelps Laboratory' }, { address: 'new address' }, function(err, docToUpdate) {
+          if(err) {throw err}; 
+        // we have the updated user returned to us
+        Listing.find({ name: 'Phelps Laboratory' }, function(err, documentData) {
+          if(err) {throw err}; 
+            console.log(documentData);
+        });
+    });
+};
+var retrieveAllListings = function() {
+  /* 
+    Retrieve all listings in the database, and log them to the console. 
+   */
+     Listing.find({}, function (err, documentData) {
+          if(err) {throw err}; 
+      console.log(documentData);
+  });
+};
+
+findLibraryWest();
+removeCable();
+updatePhelpsLab();
+retrieveAllListings();
